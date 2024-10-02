d902185283b1a7ac09a3ae6ea40ce6969602e152
Rainu Bennan <rainubennan.se@gmail.com>
2024-10-02 13:53:51 +0530

    updated onlyonnetflix.dart

--- lib/data/only_on_netflix.dart
+++ lib/data/only_on_netflix.dart
@@ -7,24 +7,122 @@ class OnlyOnNetflixData {
       "titleImage": "assets/images/StrangerThings_Title3.png",
       "releaseYear": "2024",
       "description":
-          "Reunited by their fathers death,estranged siblings with extraordinary powers uncover shockin family secrets and a looming threat to humanity.",
-      "Starring": "Elloit page , Tom Hopper ,David Castaneda,... ",
-      "Creators": "Steve Blackman",
+          "On his way home from a friend'\s house, young Will sees somthing terrifying . Neraby, a sinister secret lurks in the depths of a government lab.",
+      "Starring": "Winona Ryder , David Harbour,Millie Bobby Brown,... ",
+      "Creators": "The Duffer Brothers",
       "totalEpisodes": "",
-      "totalSeasons": "4",
+      "totalSeasons": "4 Seasons",
       "istop10": true,
       "image":
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScGRBtV6KJRRgZxhVB7yzyZXQUCLFE3kLpSw&s",
-      "episodes": {
+      "seasons": {
         "1": {
-          "episodeName": "Episode 1",
-          "description": "This is the first episode"
+          "seasonName": "Stranger Things",
+          "episodes": {
+            "1": {
+              "episodeName": "1. ChapterOne:The Vanishing of Will Byers",
+              "description":
+                  "On his way home from a friend'\s house, young Will sees somthing terrifying . Neraby, a sinister secret lurks in the depths of a government lab.",
+              "duration": "49m",
+            },
+            "2": {
+              "episodeName": "2.Chapter Two:The Weirdo on Maple Street",
+              "description":
+                  "Lucas,Mike and Dustin try to talk to the girl they found in the woods.Hopper questions an anxious Joyce about an unsettling phone call..",
+              "duration": "56m",
+            },
+            "3": {
+              "episodeName": "3.Chapter Three:Holly,Jolly",
+              "description":
+                  "An increasingly concerned Nancy looks for Barb and finds out what Jonathan'\s been up to.Joyce is convinced Will is trying to talk to her",
+              "duration": "52m",
+            },
+            "4": {
+              "episodeName": "4.Chapter Four:The Body",
+              "description":
+                  "Refusing to believe Will is dead,Joyce tries to connect with her son.The boys give Eleven a makeover.Nancy and Jonathan form an unlikely alliance.",
+              "duration": "51m",
+            },
+            "5": {
+              "episodeName": "5.Chapter Five:The Flea and the Acrobat",
+              "description":
+                  "Hopper breaks into the lab while Nancy and Jonathan confront the force that took Will.The boys ask Mr.Clarke how to travel to another dimension.",
+              "duration": "53m",
+            },
+            "6": {
+              "episodeName": "6.Chapter Six:The Monster",
+              "description":
+                  "A frantic Jonathan looks for Nancy in the darkness, but Steve'\s looking for her, too.Hoppre anf Joyce uncoverthe truth about the lab'\s experiments",
+              "duration": "47m",
+            },
+            "7": {
+              "episodeName": "7.Chapter Seven:The Bathtub",
+              "description":
+                  "Eleven struggles to reach Will, while Lucas warns that ${"the bad men are coming"}.Nancy and Jonathan show the police what Jonathan caught on camera",
+              "duration": "56m",
+            },
+            "8": {
+              "episodeName": "8.Chapter Eight:The Upside Down",
+              "description":
+                  "Dr.Brenner holds Hopper and Joyce for questioning while the boys wait with Eleven in the gym.Back at Will'\s,Nancy and jonathan prepare for battle",
+              "duration": "56m",
+            }
+          }
         },
-        "2": {
-          "episodeName": "Episode 2",
-          "description": "This is the second episode"
+        "2":{
+          "seasonName": "Stranger Things 2",
+          "episodes": {
+            "1": {
+              "episodeName": "1. ChapterOne:The Vanishing of Will Byers",
+              "description":
+                  "On his way home from a friend'\s house, young Will sees somthing terrifying . Neraby, a sinister secret lurks in the depths of a government lab.",
+              "duration": "49m",
+            },
+            "2": {
+              "episodeName": "2.Chapter Two:The Weirdo on Maple Street",
+              "description":
+                  "Lucas,Mike and Dustin try to talk to the girl they found in the woods.Hopper questions an anxious Joyce about an unsettling phone call..",
+              "duration": "56m",
+            },
+            "3": {
+              "episodeName": "3.Chapter Three:Holly,Jolly",
+              "description":
+                  "An increasingly concerned Nancy looks for Barb and finds out what Jonathan'\s been up to.Joyce is convinced Will is trying to talk to her",
+              "duration": "52m",
+            },
+            "4": {
+              "episodeName": "4.Chapter Four:The Body",
+              "description":
+                  "Refusing to believe Will is dead,Joyce tries to connect with her son.The boys give Eleven a makeover.Nancy and Jonathan form an unlikely alliance.",
+              "duration": "51m",
+            },
+            "5": {
+              "episodeName": "5.Chapter Five:The Flea and the Acrobat",
+              "description":
+                  "Hopper breaks into the lab while Nancy and Jonathan confront the force that took Will.The boys ask Mr.Clarke how to travel to another dimension.",
+              "duration": "53m",
+            },
+            "6": {
+              "episodeName": "6.Chapter Six:The Monster",
+              "description":
+                  "A frantic Jonathan looks for Nancy in the darkness, but Steve'\s looking for her, too.Hoppre anf Joyce uncoverthe truth about the lab'\s experiments",
+              "duration": "47m",
+            },
+            "7": {
+              "episodeName": "7.Chapter Seven:The Bathtub",
+              "description":
+                  "Eleven struggles to reach Will, while Lucas warns that ${"the bad men are coming"}.Nancy and Jonathan show the police what Jonathan caught on camera",
+              "duration": "56m",
+            },
+            "8": {
+              "episodeName": "8.Chapter Eight:The Upside Down",
+              "description":
+                  "Dr.Brenner holds Hopper and Joyce for questioning while the boys wait with Eleven in the gym.Back at Will'\s,Nancy and jonathan prepare for battle",
+              "duration": "56m",
+            }
+          }
         }
-      }
+      },
     },
     "2": {
       "id": 2,
@@ -41,16 +139,21 @@ class OnlyOnNetflixData {
       "istop10": true,
       "image":
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMvb_R49IH9dQcs3EMeKccfzpHh0n8cealyg&s",
-      "episodes": {
+      "seasons": {
         "1": {
-          "episodeName": "Episode 1",
-          "description": "This is the first episode"
-        },
-        "2": {
-          "episodeName": "Episode 2",
-          "description": "This is the second episode"
+          "seasonName": "Season 1",
+          "episodes": {
+            "1": {
+              "episodeName": "Episode 1",
+              "description": "This is the first episode"
+            },
+            "2": {
+              "episodeName": "Episode 2",
+              "description": "This is the second episode"
+            }
+          }
         }
-      }
+      },
     },
     "3": {
       "id": 3,
@@ -67,16 +170,21 @@ class OnlyOnNetflixData {
       "istop10": false,
       "image":
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSeHZlR9qGFtPhnTR-bquZFik4ZQlzCjI2vw&s",
-      "episodes": {
+      "seasons": {
         "1": {
-          "episodeName": "Episode 1",
-          "description": "This is the first episode"
-        },
-        "2": {
-          "episodeName": "Episode 2",
-          "description": "This is the second episode"
+          "seasonName": "Season 1",
+          "episodes": {
+            "1": {
+              "episodeName": "Episode 1",
+              "description": "This is the first episode"
+            },
+            "2": {
+              "episodeName": "Episode 2",
+              "description": "This is the second episode"
+            }
+          }
         }
-      }
+      },
     },
     "4": {
       "id": 3,
--- lib/view/home/view/single_detail_screen.dart
+++ lib/view/home/view/single_detail_screen.dart
@@ -31,123 +31,185 @@ class NetflixDetailScreen extends StatelessWidget {
           //     ))
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-              height: MediaQuery.of(context).size.height * .35,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(netflixItem['image'])),
-              )),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              const Image(
-                  height: 30,
-                  image: AssetImage('assets/images/logo_netflix.png')),
-              Text(
-                netflixItem['type'],
-                style: const TextStyle(color: Colors.white38),
-              )
-            ],
-          ),
-          Text(
-            netflixItem['name'],
-            style: const TextStyle(
-                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            "Total Seasons: ${netflixItem['totalSeasons']}",
-            style: const TextStyle(fontSize: 16),
-          ),
-          Column(
-            children: [
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      shape: BeveledRectangleBorder(),
-                      fixedSize: Size.fromHeight(50)),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.play_arrow,
-                        color: Colors.black,
-                      ),
-                      Text(
-                        'Play',
-                        style: TextStyle(color: Colors.black),
-                      )
-                    ],
-                  )),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: BeveledRectangleBorder(),
-                      backgroundColor: Colors.grey[900],
-                      fixedSize: Size.fromHeight(50)),
-                  onPressed: () {},
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.download,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Download',
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
-                  ))
-            ],
-          ),
-          Text(
-            netflixItem['description'] ?? 'No description available.',
-            style: const TextStyle(
-              fontSize: 14,
-              color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                height: MediaQuery.of(context).size.height * .35,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(netflixItem['image'])),
+                )),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                const Image(
+                    height: 30,
+                    image: AssetImage('assets/images/logo_netflix.png')),
+                Text(
+                  netflixItem['type'],
+                  style: const TextStyle(color: Colors.white38),
+                )
+              ],
             ),
-          ),
-          Row(
-            children: [
-              const Text(
-                "Starring: ",
-                style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white54,
-                    fontWeight: FontWeight.bold),
+            Text(
+              netflixItem['name'],
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Total Seasons: ${netflixItem['totalSeasons']}",
+              style: const TextStyle(fontSize: 16),
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        shape: const BeveledRectangleBorder(),
+                        fixedSize: const Size.fromHeight(50)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                        Text(
+                          'Play',
+                          style: TextStyle(color: Colors.black),
+                        )
+                      ],
+                    )),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: const BeveledRectangleBorder(),
+                        backgroundColor: Colors.grey[900],
+                        fixedSize: const Size.fromHeight(50)),
+                    onPressed: () {},
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.download,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Download',
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ))
+              ],
+            ),
+            Text(
+              netflixItem['description'] ?? 'No description available.',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
               ),
-              Text(
-                netflixItem['Starring'],
-                style: TextStyle(color: Colors.white54, fontSize: 11),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              const Text(
-                "Creator: ",
-                style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white54,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                netflixItem['Creators'],
-                style: TextStyle(color: Colors.white54, fontSize: 11),
-              )
-            ],
-          )
-
-          // Add other details as needed
-        ],
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Starring: ",
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white54,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  netflixItem['Starring'],
+                  style: TextStyle(color: Colors.white54, fontSize: 11),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Creator: ",
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white54,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  netflixItem['Creators'],
+                  style: TextStyle(color: Colors.white54, fontSize: 11),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "My List",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.thumb_up,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "Rated",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.share,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "Share",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.download,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "Download",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const Divider(
+              color: Colors.grey,
+            ),
+            
+          ],
+        ),
       ),
     );
   }
